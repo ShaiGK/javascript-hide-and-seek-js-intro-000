bab16d5f6fe10280f20368cb54e98dd0c00cce85
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-07-10 17:19:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function getFirstSelector (selector) {
   return document.querySelector(selector)
 }
+
+function nestedTarget (id, class) {
+  return document.getElementById(id).querySelector(class)
+}
