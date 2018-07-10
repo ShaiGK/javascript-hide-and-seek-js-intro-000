05f932aa9221b2da51bb296cfa690d8e65af52c8
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-07-10 18:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nestedTarget (nested, target) {
 
 function increaseRankBy (n) {
   for (var i = 0; i < document.querySelectorAll('.ranked-list').length; i++) {
-    parseInt(document.querySelectorAll('.ranked-list', 10))[i] += n
+    parseInt(document.querySelectorAll('.ranked-list'))[i] += n
   }
 }
